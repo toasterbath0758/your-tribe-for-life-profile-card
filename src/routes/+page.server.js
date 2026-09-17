fc0147b26ce0e2a0bb959c1cd2382e88a5400b9d
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-17 14:34:22 +0200

    route data ophalen server server.js #8

---
+++ src/routes/+page.server.js
@@ -0,0 +1,10 @@
+export async function load(){
+    const res = await fetch ("https://fdnd.directus.app/items/person?fields=id,name,mugshot,fav_color,fav_emoji,vibe_emoji&filter[squads][squad_id][cohort][_eq]=2627")
+    const dataPersons = await res.json();
+    
+    return {persons: dataPersons.data};
+}
+
+
+
+
