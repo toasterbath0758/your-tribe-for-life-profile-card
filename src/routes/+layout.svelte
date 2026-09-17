935606645b721b63787a4f3d9e5eb2085a902db2
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-17 14:03:11 +0200

    header met verwelkoming #5

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<header>
+	<h1>
+		WELKOM
+	</h1>
+	<h2>
+		welkom op mijn site
+	</h2>
+</header>
+
+{@render children()}
+
+<style>
+
+* {
+	margin: 0;
+	padding: 0;
+}
+
+header {
+	padding: 1em;
+	color: white;
+}
+
+
+
+
+</style>
