905aa9048640636fddc506de9f5dd6a550ce89b0
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-17 12:27:33 +0200

    positioning van de images op de rand #2

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+section {   
+    
+    position: relative;
+
+    img.left-top {
+        position: absolute;
+        top: 0;
+        left:-7.5em;
+    }
+
+        img.left-mid {
+        position: absolute;
+        top: 6em;
+        left:-7.5em;
+    }
+
+        img.left-bottom {
+        position: absolute;
+        top: 12em;
+        left:-7.5em;
+    }
+
+        img.right-top {
+        position: absolute;
+        top: 0;
+        right:-7.5em;
+    }
+
+        img.right-mid {
+        position: absolute;
+        top: 6em;
+        right:-7.5em;
+    }
+
+        img.right-bottom {
+        position: absolute;
+        top: 12em;
+        right:-7.5em;
+    }
+}
